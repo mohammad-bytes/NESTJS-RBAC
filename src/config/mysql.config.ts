6f49a64405a54ac2348d1d83cfbbe9e98688fb9f
Mohammadhusain <mohammadhusain.varaliya@bytestechnolab.com>
2023-11-28 11:15:53 +0530

    added postgresql db

--- src/config/mysql.config.ts
+++ src/config/mysql.config.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
+  type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USER,
